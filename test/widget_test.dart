92ab6047e5f4b9a69e86db7d6a89f2cbf20b7849
Shounak Mulay <shounakmulay@gmail.com>
2020-10-14 15:37:59 +0530

    Update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,20 +5,21 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter_template/data/model/consolidated_weather.dart';
+import 'package:flutter_template/data/model/weather.dart';
+import 'package:flutter_template/presentation/pages/weather/widgets/weather_list.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:kt_dart/kt.dart';
 
 import 'package:flutter_template/utils/testUtils.dart';
-import 'package:flutter_template/routes/weather_route.dart';
-import 'package:flutter_template/model/weather.dart';
-import 'package:flutter_template/model/consolidated_weather.dart';
 
 void main() {
   testWidgets('WeatherList should display city name & weather details ', (WidgetTester tester) async {
-    var weatherData = [
-      ConsolidatedWeather( 1, 'test', 'tst', 'NE', 20, 30, 27, 60, 1.008, 99, 1, 60 )];
-    var input = [Weather('Pune', weatherData)];
-    var renderForTest = RenderWithMaterial();
-    var testWidget = renderForTest.build(null,  WeatherList(input));
+    final weatherData = [
+      const ConsolidatedWeather( 1, 'test', 'tst', 'NE', 20, 30, 27, 60, 1.008, 99, 1, 60, 1 )];
+    final input = [Weather('Pune', weatherData)];
+    final renderForTest = RenderWithMaterial();
+    final testWidget = renderForTest.build(null,  WeatherList(weatherList: input.toImmutableList()));
     await tester.pumpWidget(testWidget);
 
     // the List should display the City name provided
